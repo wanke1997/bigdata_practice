f7688e122055e771c882c92fb4600c47908f0047
wanke1997 <ke_wan_2019@hotmail.com>
2023-09-01 00:39:32 -0700

    init

---
+++ src/CountTotalData.java
@@ -0,0 +1,27 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+// Total data: 4937970
+
+public class CountTotalData {
+      public static void main(String[] args) {
+            String dir = System.getProperty("user.dir")+"/data/GDS_logs.txt";
+            File fp = new File(dir);
+            try {
+                  Scanner scanner = new Scanner(fp);
+                  int cnt = 0;
+                  while(scanner.hasNextLine()) {
+                        String s = scanner.nextLine();
+                        if(cnt%10000==0) {
+                              System.out.println("example data "+(cnt/10000)+": "+s);
+                        }
+                        cnt++;
+                  }
+                  System.out.println("Total data: "+cnt);
+                  scanner.close();
+            } catch(FileNotFoundException e) {
+                  e.printStackTrace();
+            }
+      }
+}
