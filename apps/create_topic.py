2419e21259958ba8bb034934e89a29810e5b7cef
Ke Wan <ke_wan_2019@hotmail.com>
2023-09-24 21:48:48 -0700

    added error handler modules

--- apps/create_topic.py
+++ apps/create_topic.py
@@ -1,13 +1,17 @@
 from kafka.admin import KafkaAdminClient, NewTopic
 import kafka
 
-admin_client = KafkaAdminClient(
-    bootstrap_servers="kafka1:9091,kafka2:9092,kafka3:9093", 
-)
-
-topic_list = []
-topic_list.append(NewTopic(name="example_topic48", num_partitions=1, replication_factor=1))
-admin_client.create_topics(new_topics=topic_list)
+try:
+    admin_client = KafkaAdminClient(
+        bootstrap_servers="kafka1:9091,kafka2:9092,kafka3:9093", 
+    )
+    topic_list = []
+    topic_list.append(NewTopic(name="example_topic", num_partitions=1, replication_factor=1))
+    admin_client.create_topics(new_topics=topic_list)
+except kafka.errors.TopicAlreadyExistsError:
+    print("ERROR: The topic that you entered already exists.")
+else:
+    print("SUCCESS: The topic has been created.")
 
 consumer = kafka.KafkaConsumer(bootstrap_servers=['kafka1:9091','kafka2:9092','kafka3:9093'])
 topics = consumer.topics()
